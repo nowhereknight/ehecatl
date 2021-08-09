49883749d6d21b47897a4f83ad6c3621fd536d01
ZurielRod <zuriel.rodriguez@storicard.com>
2021-08-09 18:49:52 -0500

    Redirect login

--- app/routes.py
+++ app/routes.py
@@ -41,7 +41,7 @@ def login():
     :rtype: None
     """
     if current_user.is_authenticated:
-        return redirect(url_for("index"))
+        return redirect(url_for("main"))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -51,7 +51,7 @@ def login():
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get("next")
         if not next_page or url_parse(next_page).netloc != "":
-            next_page = url_for("index")
+            next_page = url_for("main")
         return redirect(next_page)
     return render_template("login.html", title="Ingresar", form=form)
 
