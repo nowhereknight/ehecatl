03df9ced45a80a4b8756515830c765273f8d8062
ZurielRod <zuriel.rodriguez@storicard.com>
2021-08-09 19:06:28 -0500

    Debug

--- app/routes.py
+++ app/routes.py
@@ -41,7 +41,7 @@ def login():
     :rtype: None
     """
     if current_user.is_authenticated:
-        return redirect(url_for("main"))
+        return redirect(url_for("main", user=current_user))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -51,7 +51,7 @@ def login():
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get("next")
         if not next_page or url_parse(next_page).netloc != "":
-            next_page = url_for("main")
+            next_page = url_for("main", user=user)
         return redirect(next_page)
     return render_template("login.html", title="Ingresar", form=form)
 
